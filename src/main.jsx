e9dbb82f75bac3a19e7fcdeed6f2cac51f7c449c
avilwock <151412286+avilwock@users.noreply.github.com>
2024-05-10 11:16:02 -0700

    started formatting folders and pages, and main.jsx structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 
+import App from './App.jsx'
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import ProfilePage from ',/pages/ProfilePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        index: true,
+        element: <ProfilePage />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
