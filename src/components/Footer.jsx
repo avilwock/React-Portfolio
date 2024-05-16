1e8c0770dbd2e5aa7f7eed4fcb9805d83a5b261f
avilwock <151412286+avilwock@users.noreply.github.com>
2024-05-16 13:45:09 -0700

    fixed footer to bottom of page, and fixed overlap issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer() {
-return (
-    <footer style={{backgroundColor: 'blue', position: 'fixed', bottom: '0', textAlign: 'center', width: '100%', height: '50px', lineHeight: '50px'}}>
+    return (
+      <footer style={{ backgroundColor: 'blue', textAlign: 'center', width: '100%', height: '50px', lineHeight: '50px', position: 'fixed', bottom: '0', left: '0', padding: '10px' }}>
         <p>Made with care by Amanda Vilwock</p>
-    </footer>
-)
-}
+      </footer>
+    );
+  }
+  
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -2,32 +2,34 @@ import { Link } from 'react-router-dom';
 
 const PortfolioItem = ({ image, title, description, link, projectLink, isSpecial, comingSoon }) => {
     return (
-        <div style={{ 
-            width: isSpecial? '800px' : '400px',
-            height: '350px',
-            border: 'groove black 5px',
-            margin: '20px 30px',
-            borderRadius: '20px',
-            display: 'inline-block',
-            textAlign: 'center',
-            verticalAlign: 'top'
-            
-        }}>
-            <img
-                src={image}
-                alt={title}
-                style={{
-                    width: '250px',
-                    height: '200px',
-                    borderRadius: '20px',
-                    marginTop: '5px',
-                }}
-            />
-            <h3 style={{ margin: '10px 0 0' }}>{title}</h3>
-            <p>{comingSoon}</p>
-            <p style={{ margin: '5px 0' }}>{description}</p>
-            <Link to={link} style={{ textDecoration: 'none', backgroundColor: 'black', color: 'blue' }}>View Repository</Link><br></br>
-            <Link to={projectLink} style={{ textDecoration: 'none', color: 'blue'}}>View Project</Link>
+        <div style={{marginBottom: '50px'}}>
+            <div style={{ 
+                width: isSpecial? '800px' : '400px',
+                height: '350px',
+                border: 'groove black 5px',
+                margin: '20px 30px',
+                borderRadius: '20px',
+                display: 'inline-block',
+                textAlign: 'center',
+                verticalAlign: 'top'
+                
+            }}>
+                <img
+                    src={image}
+                    alt={title}
+                    style={{
+                        width: '250px',
+                        height: '200px',
+                        borderRadius: '20px',
+                        marginTop: '5px',
+                    }}
+                />
+                <h3 style={{ margin: '10px 0 0' }}>{title}</h3>
+                <p>{comingSoon}</p>
+                <p style={{ margin: '5px 0' }}>{description}</p>
+                <Link to={link} style={{ textDecoration: 'none', backgroundColor: 'black', color: 'blue' }}>View Repository</Link><br></br>
+                <Link to={projectLink} style={{ textDecoration: 'none', color: 'blue'}}>View Project</Link>
+             </div>
         </div>
     );
 }
