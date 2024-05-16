b8eb8a4a9279b16f0038cacedcbefbc108dbc105
avilwock <151412286+avilwock@users.noreply.github.com>
2024-05-16 14:27:41 -0700

    moved data to db.json

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer style={{ backgroundColor: 'blue', textAlign: 'center', width: '100%', height: '50px', lineHeight: '50px', position: 'fixed', bottom: '0', left: '0', padding: '10px' }}>
+      <footer style={{ backgroundColor: 'blue', textAlign: 'center', alignItems: 'center', width: '100%', height: '30px', lineHeight: '15px', position: 'fixed', bottom: '0', left: '0', padding: '10px' }}>
         <p>Made with care by Amanda Vilwock</p>
       </footer>
     );
--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,35 +1,36 @@
 import { Link } from 'react-router-dom';
+import PortfolioData from '../../db/db.json';
 
-const PortfolioItem = ({ image, title, description, link, projectLink, isSpecial, comingSoon }) => {
+const PortfolioItem = () => {
     return (
-        <div style={{marginBottom: '50px'}}>
-            <div style={{ 
-                width: isSpecial? '800px' : '400px',
-                height: '350px',
-                border: 'groove black 5px',
-                margin: '20px 30px',
-                borderRadius: '20px',
-                display: 'inline-block',
-                textAlign: 'center',
-                verticalAlign: 'top'
-                
-            }}>
-                <img
-                    src={image}
-                    alt={title}
-                    style={{
-                        width: '250px',
-                        height: '200px',
-                        borderRadius: '20px',
-                        marginTop: '5px',
-                    }}
-                />
-                <h3 style={{ margin: '10px 0 0' }}>{title}</h3>
-                <p>{comingSoon}</p>
-                <p style={{ margin: '5px 0' }}>{description}</p>
-                <Link to={link} style={{ textDecoration: 'none', backgroundColor: 'black', color: 'blue' }}>View Repository</Link><br></br>
-                <Link to={projectLink} style={{ textDecoration: 'none', color: 'blue'}}>View Project</Link>
-             </div>
+        <div id="portfolio-projects" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            {PortfolioData.map(item => (
+                <div key={item.id} style={{ 
+                    width: item.isSpecial ? '800px' : '400px',
+                    height: '350px',
+                    border: 'groove black 5px',
+                    margin: '20px 30px',
+                    borderRadius: '20px',
+                    display: 'inline-block',
+                    textAlign: 'center',
+                    verticalAlign: 'top'
+                }}>
+                    <img
+                        src={item.image}
+                        alt={item.title}
+                        style={{
+                            width: '250px',
+                            height: '200px',
+                            borderRadius: '20px',
+                            marginTop: '5px',
+                        }}
+                    />
+                    <h3 style={{ margin: '10px 0 0' }}>{item.title}</h3>
+                    <p style={{ margin: '5px 0' }}>{item.description}</p>
+                    <Link to={item.link} style={{ textDecoration: 'none', backgroundColor: 'black', color: 'blue' }}>View Repository</Link><br></br>
+                    <Link to={item.projectLink} style={{ textDecoration: 'none', color: 'blue'}}>View Project</Link>
+                </div>
+            ))}
         </div>
     );
 }
