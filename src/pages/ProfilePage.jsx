78c9b0c12f34bde0165713250031852493b85ef3
avilwock <151412286+avilwock@users.noreply.github.com>
2024-05-16 17:03:33 -0700

    fixed profile pic

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -6,7 +6,7 @@ export default function ProfilePage() {
       <div style={{ display: 'flex', justifyContent: 'center', color: 'white' }}>
         
           <div className="container pt-4" style={{ maxWidth: '400px', textAlign: 'center' }}>
-              <img src="./src/images/Screenshot.jpg" alt="My profile picture" style={{height: '250px', border: '5px purple solid', borderRadius: '50px' }} />
+              <img src="./images/Screenshot.jpg" alt="My profile picture" style={{height: '250px', border: '5px purple solid', borderRadius: '50px' }} />
               <p>
                   My name is Amanda Vilwock, I&apos;m a new web-developer with several projects I&apos;ve created over the course of a bootcamp, as well as a few personal projects.
               </p>
