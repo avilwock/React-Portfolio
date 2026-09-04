beba34bf15f37897950df3760f17cc791f3242bd
Amanda Vilwock <avilwock@gmail.com>
2026-09-04 06:32:10 -0700

    fixing form

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -2,53 +2,45 @@ import styles from "./ContactPage.module.css";
 
 export default function ContactPage() {
   return (
-    <>
-      <form name="contact" netlify hidden>
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <textarea name="message"></textarea>
-    </form>
+    <div>
+      <h2 className={styles.contactTitle}>Contact Me</h2>
 
-      <div>
-        <h2 className={styles.contactTitle}>Contact Me</h2>
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        className={styles.myForm}
+      >
+        <input
+          className={styles.name}
+          type="text"
+          name="name"
+          placeholder="Name"
+          required
+        />
 
-        <form
-          name="contact"
-          method="POST"
-          netlify
-          className={styles.myForm}
+        <input
+          className={styles.email}
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+        />
+
+        <textarea
+          className={styles.message}
+          name="message"
+          placeholder="Please type your message"
+          required
+        />
+
+        <button
+          className={styles.button}
+          type="submit"
         >
-          <input
-            className={styles.name}
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Name"
-            required
-          />
-
-          <input
-            className={styles.email}
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Email"
-            required
-          />
-
-          <textarea
-            className={styles.message}
-            id="message"
-            name="message"
-            placeholder="Please type your message"
-            required
-          ></textarea>
-
-          <button className={styles.button} type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
-    </>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
